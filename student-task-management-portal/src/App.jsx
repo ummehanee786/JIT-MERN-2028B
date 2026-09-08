74de4ec0a60f96d8be2a39ac48f9472ab97d286f
ummehanee786 <daniyau9@gmail.com>
2026-09-08 16:26:27 +0530

    JS completed project started

---
+++ student-task-management-portal/src/App.jsx
@@ -0,0 +1,10 @@
+import Welcome from "./components/Welcome"
+
+function App(){
+  return (
+    <div>
+      <Welcome/>
+    </div>
+  );
+}
+export default App;
---
+++ student-task-management-portal/src/components/Welcome.jsx
@@ -0,0 +1,7 @@
+function Welcome(){
+    return (
+        <h1>Welcome to student task Management Portal</h1>
+    );
+}
+
+export default Welcome;
